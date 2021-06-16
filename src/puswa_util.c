4a07c83c6c73ce363ed749cda9a66db6ed2e0236
Wendrul <dethomassin.etienne@gmail.com>
2021-06-16 13:28:14 +0200

    Remove debug print, clean repository

--- src/puswa_util.c
+++ src/puswa_util.c
@@ -6,7 +6,7 @@
 /*   By: wendrul <wendrul@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/16 12:47:10 by wendrul           #+#    #+#             */
-/*   Updated: 2021/06/16 13:25:46 by wendrul          ###   ########.fr       */
+/*   Updated: 2021/06/16 13:26:18 by wendrul          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,6 @@ int	is_number(char *nbr_str)
 		error_exit(ITOA_MALLOC_FAIL, FATAL_ERROR);
 	ans = 1;
 	nb = ft_max(ft_strlen(nbr_str), ft_strlen(test_nb));
-	printf("them -%s- vs me -%s-\n", nbr_str, test_nb);
 	if (ft_strncmp(nbr_str, test_nb, nb) != 0)
 		if (nbr_str[0] != '+' || ft_strncmp(nbr_str + 1, test_nb, nb - 1) != 0)
 			ans = 0;
